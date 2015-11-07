e1359e87deaa783a352062bc55743f5ea01ed95e
rachel-1 <rachelgardner9@gmail.com>
2015-11-07 12:28:30 -0600

    Made Changes to Make More Compatible
    Sets up symbolic link and directories rather than assuming they already
    exist

--- Set-Up/full-setup.sh
+++ Set-Up/full-setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-. ./setup-people.sh # creates accounts and folders based on names.txt
-
+# create accounts and folders based on names.txt
+. ./setup-people.sh 
+# set up the symbolic link for one of the clues
+ln -s longproc putty:\ cLuE\ 4\ -\ PopUlar\ LinUx\ wEB\ sERVeR &
+# run the process for the clue
 ./putty:\ cLuE\ 4\ -\ PopUlar\ LinUx\ wEB\ sERVeR &
 
--- Set-Up/setup-people.sh
+++ Set-Up/setup-people.sh
@@ -2,46 +2,46 @@
 
 while read name
 do
-    echo -e "creating account for $name..."
-    echo -e "$name\n$name\n" | adduser $name
-    echo -e "creating folder for $name..."
-    mkdir /var/www-chapresearch/Seminar/$name
-    chown $name.$name /var/www-chapresearch/Seminar/$name
-    echo -e "adding files for $name..."
-    FILEPATH=/var/www-chapresearch/Seminar/$name
+    echo "creating account for $name..."
+    echo "$name\n$name\n" | adduser $name
+    echo "creating folder for $name..."
+    mkdir -p ../ScavengerHunt/HomeDirectories/$name
+    chown $name.$name ../ScavengerHunt/HomeDirectories/$name
+    echo "adding files for $name..."
+    FILEPATH=../ScavengerHunt/HomeDirectories/$name
 
     if [ ! -f $FILEPATH/$name.html ] 
-    then echo "hi" > /var/www-chapresearch/Seminar/$name/$name.html
+    then echo "hi" > $FILEPATH/$name.html
     else echo "$name.html already exists"
     fi
-    chown $name.$name /var/www-chapresearch/Seminar/$name/$name.html 
-    chmod 774 /var/www-chapresearch/Seminar/$name/$name.html
+    chown $name.$name $FILEPATH/$name.html 
+    chmod 774 $FILEPATH/$name.html
 
     if [ ! -f $FILEPATH/$name-1.html ]
-    then cp /var/www-chapresearch/Seminar/format.html /var/www-chapresearch/Seminar/$name/$name-1.html
+    then cp format.html $FILEPATH/$name-1.html
     else echo "$name-1.html already exists"
     fi
-    chown $name.$name /var/www-chapresearch/Seminar/$name/$name-1.html 
-    chmod 774 /var/www-chapresearch/Seminar/$name/$name-1.html
+    chown $name.$name $FILEPATH/$name-1.html 
+    chmod 774 $FILEPATH/$name-1.html
 
     if [ ! -f $FILEPATH/$name-2.html ]
-    then cp /var/www-chapresearch/Seminar/form_format.html /var/www-chapresearch/Seminar/$name/$name-2.html
+    then cp form_format.html $FILEPATH/$name-2.html
     else echo "$name-2.html already exists"
     fi
-    chown $name.$name /var/www-chapresearch/Seminar/$name/$name-2.html 
-    chmod 774 /var/www-chapresearch/Seminar/$name/$name-2.html
+    chown $name.$name $FILEPATH/$name-2.html 
+    chmod 774 $FILEPATH/$name-2.html
 
     if [ ! -f $FILEPATH/$name-3.php ]
-    then cp /var/www-chapresearch/Seminar/format.php /var/www-chapresearch/Seminar/$name/$name-3.php
+    then cp format.php $FILEPATH/$name-3.php
     else echo "$name-3.php already exists"
     fi
-    chown $name.$name /var/www-chapresearch/Seminar/$name/$name-3.php
-    chmod 774 /var/www-chapresearch/Seminar/$name/$name-3.php
+    chown $name.$name $FILEPATH/$name-3.php
+    chmod 774 $FILEPATH/$name-3.php
 
     if [ ! -f $FILEPATH/$name-4.php ]
-    then cp /var/www-chapresearch/Seminar/form_format.php /var/www-chapresearch/Seminar/$name/$name-4.php
+    then cp form_format.php $FILEPATH/$name-4.php
     else echo "$name-4.php already exists"
     fi
-    chown $name.$name /var/www-chapresearch/Seminar/$name/$name-4.php
-    chmod 774 /var/www-chapresearch/Seminar/$name/$name-4.php
+    chown $name.$name $FILEPATH/$name-4.php
+    chmod 774 $FILEPATH/$name-4.php
 done < names.txt
